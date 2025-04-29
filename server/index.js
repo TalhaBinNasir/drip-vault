dbe1a1b5cdfbd4c0d5a646ccb5b66e2ba34244bc
talhabinnasir <talhaabinnasir@gmail.com>
2025-04-29 16:49:10 +0500

    redeploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import { typeDefs } from "./graphql/typeDefs.js";
 import resolvers from "./graphql/resolvers/index.js";
 import connectDB from "./db/connect.js";
@@ -13,6 +14,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
@@ -23,7 +32,7 @@ const PORT = process.env.PORT || 8000;
 
 const startServer = async () => {
   await apolloServer.start();
-  apolloServer.applyMiddleware({ app: app });
+  apolloServer.applyMiddleware({ app: app, cors: false });
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(PORT, () => console.log("Server is running"));
